ae7250b00af181e4f3d45f632f94cc8e30c8d411
PavelBlessYou51 <prokoshevpavel@mail.ru>
2024-10-20 16:12:24 +0300

    Домашнее задание урок №3 Side and exsitsting test

--- HomeWorks/src/main/java/homework/lesson2/Triangle.java
+++ HomeWorks/src/main/java/homework/lesson2/Triangle.java
@@ -4,6 +4,16 @@ import static java.lang.Math.sqrt;
 
 public record Triangle(double a, double b, double c) {
 
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("The side of triangle should be non-negative!");
+        }
+        if (a + b > c || a + c > b || c + b > a) {
+            throw new IllegalArgumentException("The triangle does not exist!");
+        }
+
+    }
+
     double perimetr() {
         return this.a + this.b + this.c;
     }
--- HomeWorks/src/test/java/homework/lesson2/TriangleTest.java
+++ HomeWorks/src/test/java/homework/lesson2/TriangleTest.java
@@ -18,4 +18,25 @@ public class TriangleTest {
         Assertions.assertEquals(6.0, t.square());
     }
 
+    @Test
+    void negativeSideTest() {
+        try {
+            Triangle t = new Triangle(-3.0, 4.0, 5.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // "OK"
+        }
+
+    }
+
+    @Test
+    void existingTringle() {
+        try {
+            Triangle t = new Triangle(100, 4, 7);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // "OK"
+        }
+
+    }
 }
